ebeafe01f7c7795d71d580409c70049a5fef4299
Intomies <valtteri.nummi@gmail.com>
2021-04-14 14:56:25 +0300

    added variables for different colors and for healthbar settings

--- settings.py
+++ settings.py
@@ -19,18 +19,38 @@ FIGHTER_START_POSITIONS = {
         700
     ]
 }
-
 FIGHTER_HEIGHT = {
     "Knight": 260,
     "Bandit": 270
 }
-
 FIGHTER_IMAGE_SCALE = 3
-# FIGHTER_ACTIONS = {
-#     "Attack": "Attack",
-#     "Death": "Death",
-#     "Hurt": "Hurt",
-#     "Idle": "Idle"
-# }
+FIGHTER_ACTIONS = [
+    "attack", 
+    "death", 
+    "hurt", 
+    "idle"
+]
+MAX_AMOUNT_OF_ENEMIES = 3
+MIN_AMOUNT_OF_ENEMIES = 1
 
-FIGHTER_ACTIONS = ["attack", "death", "hurt", "idle"]
+# Interface settings
+
+HEALTH_BAR_POSITIONS_X = {
+    "Knight": 100,
+    "Bandit": 500
+}
+HEALTH_BAR_POSITIONS_Y = [
+    SCREEN_HEIGHT - BOTTOM_PANEL + 30,
+    SCREEN_HEIGHT - BOTTOM_PANEL + 70,
+    SCREEN_HEIGHT - BOTTOM_PANEL + 110
+]
+HEALTH_BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 150
+
+# Colors settings
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
