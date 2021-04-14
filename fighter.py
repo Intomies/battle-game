ecea15ed2ddca057b27aae8f4661e2132d64f03a
Intomies <valtteri.nummi@gmail.com>
2021-04-14 10:53:40 +0300

    changed all the class variables back to Fighter-class

--- fighter.py
+++ fighter.py
@@ -6,8 +6,31 @@ from settings import *
 
 class Fighter:
 
-    def __init__(self):
-        pass
+    def __init__(self, char_type, max_hp, strength, potions, count):
+
+        self.char_type = char_type
+        self.max_hp = max_hp
+        self.strength = strength
+        self.count = count
+        
+        self.hp = max_hp
+        self.potions = potions
+        self.start_potions = potions
+        self.alive = True
+        self.actions = FIGHTER_ACTIONS # [0: attack, 1: death, 2: hurt, 3: idle]
+        
+        self.current_action = "idle"
+        self.current_frame_index = 0
+        self.update_time = pg.time.get_ticks()
+        self.animation_cooldown = 100
+
+        self.starting_positions = FIGHTER_START_POSITIONS
+        self.fighter_height = FIGHTER_HEIGHT
+        self.image_scale = FIGHTER_IMAGE_SCALE
+        self.images_list = self.load_images()
+        self.image = self.images_list[self.current_action][self.current_frame_index]
+        self.rect = self.get_rect()
+        self.rect.center = (self.get_position())
 
     def update(self):
 
