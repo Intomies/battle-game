dad2bbd11a7363ab99d98cb4b6babb1d7fd68371
Intomies <valtteri.nummi@gmail.com>
2021-04-14 14:53:13 +0300

    added positional variables for Knight-classes healthbar to use as a parameter when creating the healthbar

--- knight.py
+++ knight.py
@@ -10,3 +10,6 @@ class Knight(Fighter):
     def __init__(self, char_type, max_hp, strength, potions, count):
         
         super().__init__(char_type, max_hp, strength, potions, count)
+
+        self.health_bar_pos_x = HEALTH_BAR_POSITIONS_X["Knight"]
+        self.health_bar_pos_y = HEALTH_BAR_POSITIONS_Y[0]
